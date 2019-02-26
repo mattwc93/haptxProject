b1a8237edec98efaead3371b7d443c7ce9b4a9a7
Matt Clark <mattwc93@gmail.com>
2019-02-25 20:38:03 -0800

    first commit

---
+++ Assets/Scripts/ChargedParticles/ParticleBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBehavior : MonoBehaviour
+{
+  #region Global Vars
+  [Range(-1, 1)]
+  public float charge;
+  private Rigidbody rb;
+  private float electroStaticConstant = 5f;
+  #endregion
+  void Start()
+  {
+    // Add this particle to the Particle Trackers list:
+    ParticleTracker.instance.AddParticle(gameObject);
+  }
+  // AddElectroStaticForce method does the calculations for the direction and magnitude of force to apply to another particle, then applies that force.
+  // Takes a full particle Game Object as the argument so we can access multiple parts of the particle from this one method:
+  void AddElectroStaticForce(GameObject particle)
+  {
+    if (particle != null)
+    {
+      // Grab the components we need to access on the particle we are influencing:
+      Rigidbody otherRB = particle.GetComponent<Rigidbody>();
+      float otherCharge = particle.GetComponent<ParticleBehavior>().charge;
+      // Calculate the distance between the particles
+      float distance = Vector3.Distance(transform.position, particle.transform.position);
+      // Get a vector pointing from this particle to the other particle, divide by distance to normalize the vector:
+      Vector3 direction = (particle.transform.position - transform.position) / distance;
+      // Use Coulomb's Law to calculate the magnitude of the force placed on the particle, because this is a "scaled up" simlation and
+      // we are using -1 to 1 as our range for charge to keep things simple, the electrostatic constant is just a float we can adjust as we like(replaces Coulomb's constant).
+      float electroStaticForce = (electroStaticConstant * charge * otherCharge) / (distance * distance);
+      // Add the force to the rigidbody of the other particle, we only add it to the other particle because the other particle will add the force back on it's own.
+      // We normalize the direction vector because we want to use the force magnitude we calculated here to determine the magnitude of the force vector applied:
+      otherRB.AddForce(direction * electroStaticForce);
+    }
+  }
+  void FixedUpdate()
+  {
+    // Loop through all other particles in the Particle Tracker, and if they aren't the particle this script is attached to we call our method to calculate and add our force:
+    foreach (GameObject particle in ParticleTracker.instance.particles)
+    {
+      if (particle.name != name)
+      {
+        AddElectroStaticForce(particle);
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/ChargedParticles/ParticleTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTracker : MonoBehaviour
+{
+  #region Singleton
+  // Singleton pattern to keep 1 single list of all particles in the scene, that is also easily accessible by those particles behavior scripts.
+  // Saves us time by avoiding having to search for the list of all particles every time by tag or name.
+  public static ParticleTracker instance;
+  #endregion
+  // List of gameobjects to store all the particles in the scene, particles will add themselves to this list in their start method.
+  public List<GameObject> particles;
+  void Awake()
+  {
+    // Singleton pattern:
+    instance = this;
+    // Instantiate our particle list:
+    particles = new List<GameObject>();
+  }
+
+  // Public method so particles can add themselves to this list when they are created using our singleton class:
+  public void AddParticle(GameObject particle)
+  {
+    particles.Add(particle);
+  }
+}
