a5cc81c4d21cf5fc98c47209e0f2e17bb7d6bf27
Matt Clark <mattwc93@gmail.com>
2019-02-26 20:06:00 -0800

    final submission

---
+++ Assets/Scripts/ChargedParticles/EditorExtensions/ParticleMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+// Editor extension script to add our prefab particles to the right click menu in the heirarchy window:
+public class CreateMenu : MonoBehaviour
+{
+  private static System.Random random = new System.Random();
+  private static int randomRange = 5;
+  // Adds drop down option to heirarchy menu for the particle
+  [MenuItem("GameObject/Particles/Proton", false, 10)]
+  static void CreateProton()
+  {
+    // Load our prefab from our resources folder
+    Object proton = Resources.Load("Proton");
+    // Create a random position vector
+    Vector3 position = new Vector3(random.Next(-randomRange, randomRange + 1), random.Next(-randomRange, randomRange + 1), random.Next(-randomRange, randomRange + 1));
+    // Instantiate the prefab at the set position
+    Instantiate(proton, position, Quaternion.identity);
+  }
+
+  [MenuItem("GameObject/Particles/Electron", false, 10)]
+  static void CreateElectron()
+  {
+    Object electron = Resources.Load("Electron");
+    Vector3 position = new Vector3(random.Next(-randomRange, randomRange + 1), random.Next(-randomRange, randomRange + 1), random.Next(-randomRange, randomRange + 1));
+    Instantiate(electron, position, Quaternion.identity);
+  }
+
+  [MenuItem("GameObject/Particles/Neutron", false, 10)]
+  static void CreateNeutron()
+  {
+    Object neutron = Resources.Load("Neutron");
+    Vector3 position = new Vector3(random.Next(-randomRange, randomRange + 1), random.Next(-randomRange, randomRange + 1), random.Next(-randomRange, randomRange + 1));
+    Instantiate(neutron, position, Quaternion.identity);
+  }
+
+  // Menu item to generate a bunch of protons and electrons. Set the total number of particles you want with the numParticles variable.
+  [MenuItem("GameObject/Particles/Random Particle System", false, 10)]
+  static void CreateRandomParticleSystem()
+  {
+    int numParticles = 50;
+
+    for (int i = 0; i < numParticles; i++)
+    {
+      float particleToCreate = random.Next(0, 2);
+      switch (particleToCreate)
+      {
+        case 0f:
+          CreateProton();
+          break;
+        case 1f:
+          CreateElectron();
+          break;
+        default:
+          break;
+      }
+
+    }
+  }
+}
+
--- Assets/Scripts/ChargedParticles/ParticleBehavior.cs
+++ Assets/Scripts/ChargedParticles/ParticleBehavior.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class ParticleBehavior : MonoBehaviour
 {
   #region Global Vars
+  // Range for our charges, can be altered here to allow larger magnitude charges
   [Range(-1, 1)]
   public float charge;
   private Rigidbody rb;
   Color chargeColor;
   Color finalColor;
+  // Arbitrary representation of Coulomb's Constant.
   private float electroStaticConstant = 5f;
   #endregion
   void Start()
@@ -34,11 +36,12 @@ public class ParticleBehavior : MonoBehaviour
       // we are using -1 to 1 as our range for charge to keep things simple, the electrostatic constant is just a float we can adjust as we like(replaces Coulomb's constant).
       float electroStaticForce = (electroStaticConstant * charge * otherCharge) / (distance * distance);
       // Add the force to the rigidbody of the other particle, we only add it to the other particle because the other particle will add the force back on it's own.
-      // We normalize the direction vector because we want to use the force magnitude we calculated here to determine the magnitude of the force vector applied:
+      // We normalize the direction vector because we want to use the force magnitude we calculated here to determine the magnitude of the force vector applied. If both
+      // charges are positive or negative, the force will be positive and repulse. If they are opposite then the result will be a negative force and attract:
       otherRB.AddForce(direction * electroStaticForce);
     }
   }
-  void Update()
+  void LateUpdate()
   {
     // Code to check our charge and assign a color based on it. Negative charge will lerp between white and blue, and positive between white
     // and red. Magnitude of charge will determine how blue or red the particle is.
@@ -61,12 +64,12 @@ public class ParticleBehavior : MonoBehaviour
 
     // Set the mass of a particle if it has a positive charge
   }
-  void FixedUpdate()
+  void Update()
   {
     // Loop through all other particles in the Particle Tracker, and if they aren't the particle this script is attached to we call our method to calculate and add our force:
     foreach (GameObject particle in ParticleTracker.instance.particles)
     {
-      if (particle.name != name)
+      if (!GameObject.ReferenceEquals(particle, gameObject))
       {
         AddElectroStaticForce(particle);
       }
